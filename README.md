189e6e5919088bce9e0a52e5b5e876973a71ed08
Sn1cke <113979281+Sn1cke@users.noreply.github.com>
2023-01-11 11:17:47 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# evil-scientist
+A simple program to introduce TS to vocational university students
+
+## Beskrivning av uppgiften
+
+(kommer)
+
+## Technologies
+
+- Typescript
+- HTML
+- CSS
---
+++ src/app.ts
@@ -0,0 +1,57 @@
+/*
+const arrScientists: scientists[] = [
+    {
+    name: true,
+    age: 36,
+    henchmen: 2,
+    description: "Stark som fan",
+    },
+    {
+    name: "Ollof",
+    age: 14,
+    henchmen: 2,
+    description: "Number1234",
+    },
+];
+
+const numberArr: number[] = [1, "x", 3, 4];
+*/
+
+// ====================================================
+
+
+type Scientist = {
+    name: string,
+    age: number,
+    henchmen: number,
+    description: string,
+}
+
+let scientistDB: {
+    scientists: Scientist[],
+    addScientist: (n: string, a: number, h: number, d: string) => void
+} = {
+    scientists: [
+        {
+        name: "Victor Frankenstein",
+        age: 37,
+        henchmen: 2,
+        description: "Likes to write poetry",
+        },
+        {
+        name: "Alfred Nobel",
+        age: 26,
+        henchmen: 1,
+        description: "Killed his brother in a factory accident",
+        }
+    ],
+    addScientist: function (name: string, age: number, henchmen: number, description: string): void {
+        const x: Scientist = {
+            name: name,
+            age: age,
+            henchmen: henchmen,
+            description: description,
+        }
+        this.scientists.push(x)
+    }
+}
