295c32763d2e8098a40926c7b7cf3e4fcf0a8ab0
Jesper Flood <flod96@hotmail.com>
2023-01-11 11:35:21 +0100

    Test av Jeremy

--- src/app.ts
+++ src/app.ts
@@ -19,41 +19,45 @@ const numberArr: number[] = [1, "x", 3, 4];
 
 // ====================================================
 
-
 type Scientist = {
+  name: string;
+  age: number;
+  henchmen: number;
+  description: string;
+};
+
+let scientistDB: {
+  scientists: Scientist[];
+  addScientist: (n: string, a: number, h: number, d: string) => void;
+} = {
+  scientists: [
+    {
+      name: "Victor Frankenstein",
+      age: 37,
+      henchmen: 2,
+      description: "Likes to write poetry",
+    },
+    {
+      name: "Alfred Nobel",
+      age: 26,
+      henchmen: 1,
+      description: "Killed his brother in a factory accident",
+    },
+  ],
+  addScientist: function (
     name: string,
     age: number,
     henchmen: number,
-    description: string,
-}
+    description: string
+  ): void {
+    const x: Scientist = {
+      name: name,
+      age: age,
+      henchmen: henchmen,
+      description: description,
+    };
+    this.scientists.push(x);
+  },
+};
 
-let scientistDB: {
-    scientists: Scientist[],
-    addScientist: (n: string, a: number, h: number, d: string) => void
-} = {
-    scientists: [
-        {
-        name: "Victor Frankenstein",
-        age: 37,
-        henchmen: 2,
-        description: "Likes to write poetry",
-        },
-        {
-        name: "Alfred Nobel",
-        age: 26,
-        henchmen: 1,
-        description: "Killed his brother in a factory accident",
-        }
-    ],
-    addScientist: function (name: string, age: number, henchmen: number, description: string): void {
-        const x: Scientist = {
-            name: name,
-            age: age,
-            henchmen: henchmen,
-            description: description,
-        }
-        this.scientists.push(x)
-    }
-}
-
-// Testar lite ändringar
+// Jeremy Test
